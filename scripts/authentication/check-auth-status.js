cb940c8f156d7b797a17417c78a5915ee30c0885
Johnny <johnlabedzki@gmail.com>
2023-11-19 15:17:46 -0800

    add access.html page

--- scripts/authentication/check-auth-status.js
+++ scripts/authentication/check-auth-status.js
@@ -5,7 +5,6 @@ import { API_URL } from "../../constants.js";
     const response = await axios.get(`${API_URL}/who-am-i`);
     console.log("response.data: ", response.data);
   } catch (err) {
-    console.log(err.response.status);
     if (err.response.status === 401) {
       // instead of redirecting, just show login and register button
       console.log("you are not logged in");
