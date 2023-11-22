91ee5ed9f65dbf4b0bc53c341e1a36adbe6571e4
Johnny <johnlabedzki@gmail.com>
2023-11-22 08:00:54 -0800

    finish styling and logic of nav bar

--- scripts/authentication/check-auth-status.js
+++ scripts/authentication/check-auth-status.js
@@ -1,15 +1,19 @@
 import { API_URL } from "../../constants.js";
 
+/**
+ * Use querySelectorAll to account for the fact that there is an element in both
+ * the main nav and the mobile nav.
+ */
 const navAccount = document.querySelectorAll(".header__nav-account");
 const navAccess = document.querySelectorAll(".header__nav-access");
 
 (async () => {
   try {
     await axios.get(`${API_URL}/who-am-i`);
-    navAccount.forEach((e) => (e.style.visibility = "visible"));
+    navAccount.forEach((e) => (e.style.display = "flex"));
   } catch (err) {
     if (err.response.status === 401) {
-      navAccess.forEach((e) => (e.style.visibility = "visible"));
+      navAccess.forEach((e) => (e.style.display = "flex"));
     }
   }
 })();
